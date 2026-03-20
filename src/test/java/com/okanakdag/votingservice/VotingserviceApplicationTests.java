a302af04126e90b83bb21b5b5494ed5571f5b133
Okan Akdag <mertakdag08@gmail.com>
2026-03-20 22:26:09 +0300

    create project

---
+++ src/test/java/com/okanakdag/votingservice/VotingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okanakdag.votingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
